c5776321f91e17c9f010de7b2b86893707bf64b7
jneirar <jorge.neira@utec.edu.pe>
2021-10-20 16:47:56 -0500

    First test

---
+++ src/main/java/ing/Main.java
@@ -0,0 +1,7 @@
+package ing;
+
+public class Main {
+    public static void main(String[] args){
+    //Nothing
+    }
+}
---
+++ src/test/java/ing/MainTest.java
@@ -0,0 +1,11 @@
+package ing;
+
+import org.junit.Test;
+import org.testng.Assert;
+
+public class MainTest {
+    @Test()
+    public void testMain(){
+        Main main = new Main();
+    }
+}
